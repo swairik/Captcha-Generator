0eca4886dbf49d147e3442bb768ea044b19ce674
Swairik <swairikdey1001@gmail.com>
2022-08-24 04:15:12 +0530

    created 3 working versions....with lines, linear gradient repeat, linear gradient mirror created update button...updates everything

--- app/src/main/java/com/example/captchagenerator/generator.java
+++ app/src/main/java/com/example/captchagenerator/generator.java
@@ -2,11 +2,6 @@ package com.example.captchagenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class generator extends AppCompatActivity {
-    Button btn;
+    Button btn, updateBtn;
     TextView res;
     EditText value;
-    ImageView captcha;
+    ImageView captcha, captcha2, captcha3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +22,11 @@ public class generator extends AppCompatActivity {
         setContentView(R.layout.activity_generator);
 
         btn = findViewById(R.id.checkBtn);
+        updateBtn = findViewById(R.id.updateBtn);
         res = findViewById(R.id.res);
         captcha = findViewById(R.id.captcha);
+        captcha2 = findViewById(R.id.captcha2);
+        captcha3 = findViewById(R.id.captcha3);
         value = findViewById(R.id.value);
 
         int height = captcha.getDrawable().getIntrinsicHeight();
@@ -49,22 +47,39 @@ public class generator extends AppCompatActivity {
 //        canvas.drawText("Hello", text_x, text_y, paint);
 
 //        textCaptcha captchaObj = new textCaptcha(6);
-        textCaptcha captchaObj = new textCaptcha(height, width,6);
-        captcha.setImageBitmap(captchaObj.image);
+        final textCaptcha[] captchaObj = {new textCaptcha(height, width, 6)};
+        captcha.setImageBitmap(captchaObj[0].image1);
+        captcha2.setImageBitmap(captchaObj[0].image2);
+        captcha3.setImageBitmap(captchaObj[0].image3);
+
+        res.setText(captchaObj[0].ans);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String text = value.getText().toString();
-                if(text.equals(captchaObj.ans)) {
-                    res.setText("Correct!!!");
+                if(text.equals(captchaObj[0].ans)) {
+//                    res.setText("Correct!!!");
                     Toast.makeText(generator.this, "Correct", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    res.setText("Incorrect!!!");
+//                    res.setText("Incorrect!!!");
                     Toast.makeText(generator.this, "Incorrect", Toast.LENGTH_SHORT).show();
                 }
+
+            }
+        });
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                captchaObj[0] = new textCaptcha(height, width, 6);
+                captcha.setImageBitmap(captchaObj[0].image1);
+                captcha2.setImageBitmap(captchaObj[0].image2);
+                captcha3.setImageBitmap(captchaObj[0].image3);
+                value.getText().clear();
+                res.setText(captchaObj[0].ans);
 
             }
         });
--- app/src/main/java/com/example/captchagenerator/textCaptcha.java
+++ app/src/main/java/com/example/captchagenerator/textCaptcha.java
@@ -7,70 +7,17 @@ import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Shader;
 
-import java.util.*;
 import java.util.Random;
 
 public class textCaptcha {
-    Bitmap image;
+    Bitmap image1, image2, image3;
     String ans = "";
     int width;
     int height;
 
-    //    wrapper constructor
-    textCaptcha(int wordLength) {
-        new textCaptcha(300, 300, wordLength);
-    }
+    //    static List usedColors;
 
-    textCaptcha(int height, int width, int wordLength) {
-        image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(image);
-
-//        Paint paint = new Paint();
-//        paint.setColor(Color.BLACK);
-//        paint.setTextSize(60);
-//        float text_x = 50;
-//        float text_y = 50;
-//        canvas.drawText("Hello11111111111111111111111111111111111111111111111111111111111111", text_x, text_y, paint);
-
-        StringBuilder temp = new StringBuilder();
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
-        for(int i = 0; i < wordLength; i++) {
-            int index = (int)(AlphaNumericString.length() * Math.random());
-            temp.append(AlphaNumericString.charAt(index));
-        }
-        ans = String.valueOf(temp);
-//        canvas.drawColor(Color.YELLOW);
-//        canvas.drawText(String.valueOf(ans), text_x, text_y, paint);
-
-        Random r = new Random(System.currentTimeMillis());
-
-        Paint p = new Paint();
-        LinearGradient gradient = new LinearGradient(0, 0, width / wordLength, height / 2, color(), color(), Shader.TileMode.MIRROR);
-        p.setShader(gradient);
-
-        canvas.drawRect(0, 0, width, height, p);
-
-        Paint paint = new Paint();
-        paint.setTextSize(60);
-//        paint.setTextSize(width / height * 20);
-
-        char[] data = ans.toCharArray();
-        int x = 0, y = 0;
-        for (int i = 0; i < data.length; i++) {
-            x += (30 - (3 * wordLength)) + (Math.abs(r.nextInt()) % (65 - (1.2 * wordLength)));
-            y = 50 + Math.abs(r.nextInt()) % 50;
-            Canvas cc = new Canvas(image);
-            paint.setTextSkewX(r.nextFloat() - r.nextFloat());
-            paint.setColor(color());
-            cc.drawText(data, i, 1, x, y, paint);
-            paint.setTextSkewX(0);
-        }
-
-    }
-
-//    static List usedColors;
-
-    public static int color(){
+    public static int getRandomColor(){
         Random r = new Random();
         int number;
 //        do{
@@ -90,6 +37,86 @@ public class textCaptcha {
             case 8: return Color.YELLOW;
             case 9: return Color.WHITE;
             default: return Color.WHITE;
+        }
+    }
+
+    //    wrapper constructor
+    textCaptcha(int wordLength) {
+        new textCaptcha(300, 300, wordLength);
+    }
+
+    textCaptcha(int height, int width, int wordLength) {
+        image1 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        image2 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        image3 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(image1);
+        Canvas canvas2 = new Canvas(image2);
+        Canvas canvas3 = new Canvas(image3);
+
+//        Paint paint = new Paint();
+//        paint.setColor(Color.BLACK);
+//        paint.setTextSize(60);
+//        float text_x = 50;
+//        float text_y = 50;
+//        canvas.drawText("Hello11111111111111111111111111111111111111111111111111111111111111", text_x, text_y, paint);
+
+        StringBuilder temp = new StringBuilder();
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+        for(int i = 0; i < wordLength; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            temp.append(AlphaNumericString.charAt(index));
+        }
+        ans = String.valueOf(temp);
+//        canvas.drawColor(Color.YELLOW);
+//        canvas.drawText(String.valueOf(ans), text_x, text_y, paint);
+
+        Random random = new Random(System.currentTimeMillis());
+
+        Paint p = new Paint();
+        p.setColor(Color.WHITE);
+        canvas.drawRect(0, 0, width, height, p);
+
+        //        p.setColor(Color.BLACK);
+        int numLines = 4 + (Math.abs(random.nextInt())) % 8;
+        for(int i = 0; i < numLines; i++) {
+//            p.setColor(getRandomColor());
+            p.setColor(Color.BLACK);
+            int startX = Math.abs(random.nextInt(width));
+            int startY = Math.abs(random.nextInt(height));
+            int stopX = Math.abs(random.nextInt(width));
+            int stopY = Math.abs(random.nextInt(height));
+            canvas.drawLine(startX, startY, stopX, stopY, p);
+//            canvas.drawLine(0, 0, 100, 100, p);
+        }
+
+        Paint p2 = new Paint();
+        LinearGradient gradient = new LinearGradient(0, 0, width / wordLength, height / 2, getRandomColor(), getRandomColor(), Shader.TileMode.MIRROR);
+        p2.setShader(gradient);
+        canvas2.drawRect(0, 0, width, height, p2);
+
+        Paint p3 = new Paint();
+        LinearGradient gradient2 = new LinearGradient(0, 0, width / wordLength, height / 2, getRandomColor(), getRandomColor(), Shader.TileMode.REPEAT);
+        p3.setShader(gradient2);
+        canvas3.drawRect(0, 0, width, height, p3);
+
+        Paint paint = new Paint();
+        paint.setTextSize(60);
+
+        int x = 0, y = 0;
+        for(char ch : ans.toCharArray()) {
+//            x += (30 - (3 * wordLength)) + (Math.abs(random.nextInt()) % (65 - (1.2 * wordLength)));
+//            need to restrict the spacing
+            x += (wordLength * 2) + (Math.abs(random.nextInt()) % (60 - (2 * wordLength)));
+            y = 50 + Math.abs(random.nextInt()) % 100;
+            Canvas cc = new Canvas(image1);
+            Canvas cc2 = new Canvas(image2);
+            Canvas cc3 = new Canvas(image3);
+            paint.setTextSkewX(random.nextFloat() - random.nextFloat());
+            paint.setColor(getRandomColor());
+            cc.drawText(String.valueOf(ch), x, y, paint);
+            cc2.drawText(String.valueOf(ch), x, y, paint);
+            cc3.drawText(String.valueOf(ch), x, y, paint);
         }
     }
 
